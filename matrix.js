a16647c1636db536861e9a24889ba6e2f33ba035
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-04-21 09:22:05 +0300

    suma calculata

--- matrix.js
+++ matrix.js
@@ -9,3 +9,15 @@ const matrice2 = [
   [4, 5, 6],
   [7, 8, 9],
 ];
+
+function sumaMatrix(matrice1, matrice2) {
+  let matriceSuma = [];
+  for (let i = 0; i < matrice1.length; i++) {
+    matriceSuma.push([]);
+    for (let j = 0; j < matrice1[i].length; j++)
+      matriceSuma[i].push(matrice1[i][j] + matrice2[i][j]);
+  }
+  return matriceSuma;
+}
+
+console.log(sumaMatrix(matrice1, matrice2));
